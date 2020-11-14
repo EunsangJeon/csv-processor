a2ae661f7ede6b75f10c3c74de08ad345d31613d
Eunsang <nicolaus1207@gmail.com>
2020-11-14 17:46:51 +0900

    read and parse csv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('resources/football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+console.log(matches);
