200e7388c3253b8dbe5dacf6bb79209156c0f852
Eunsang <nicolaus1207@gmail.com>
2020-11-15 14:47:30 +0900

    compoisition way

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('resources/football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('resources/football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (
     (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
     (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
